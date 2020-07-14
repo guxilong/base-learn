51cb93217091bfe8103da2c33af94510af86c02b
guxilong <963435525@qq.com>
2020-07-14 23:35:46 +0800

    add

--- JavaProject-gradle/src/main/java/com/guxilong/FirstDemo.java
+++ JavaProject-gradle/src/main/java/com/guxilong/FirstDemo.java
@@ -3,10 +3,11 @@ package com.guxilong;
 /**
  * @author guxl
  * @version 1.0
- * @date 2020/6/9 12:11 上午
+ * @date 2020/7/14 10:54 下午
  */
-public class test2 {
+public class FirstDemo {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("First gradle project!");
     }
 }
+
--- java-learn/src/com/guxilong/test1.java
+++
@@ -1,9 +0,0 @@
-package com.guxilong;
-
-/**
- * @author guxl
- * @version 1.0
- * @date 2020/6/9 12:09 上午
- */
-public class test1 {
-}
