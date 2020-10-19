9d7faf8166cd6e8a4b372aa64d48fbe77e2e08b2
guxilong <963435525@qq.com>
2020-10-19 13:53:55 +0800

    test add log

--- java-learn/src/main/java/com/guxilong/Test1.java
+++ java-learn/src/main/java/com/guxilong/Test1.java
@@ -9,5 +9,8 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("test main");
         System.out.println("test main");
+        System.out.println("test main");
+        System.out.println("test main");
+
     }
 }
