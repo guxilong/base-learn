be34ec3d986665a53909540576ee74a22248d2ce
guxilong <963435525@qq.com>
2020-10-19 11:16:29 +0800

    add src

---
+++ java-learn/src/main/java/com/guxilong/Test1.java
@@ -0,0 +1,12 @@
+package main.java.com.guxilong;
+
+/**
+ * @author guxl
+ * @version 1.0
+ * @date 2020/10/19 11:15 上午
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("test main");
+    }
+}
